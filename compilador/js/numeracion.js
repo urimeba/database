15ebe4568e70e94319caa1f5925c0a7e20cd2745
Carlos Manuel Sanchez Martinez <43074684+Carlos5anchez@users.noreply.github.com>
2020-03-03 11:15:45 -0600

    dsfsdfs

--- compilador/js/numeracion.js
+++ compilador/js/numeracion.js
@@ -1,13 +1,67 @@
-window.addEventListener('load', numerar, false);
-function numerar() {
+window.addEventListener('load', cambiarTextArea, false);
+function cambiarTextArea() {
 
-var contenedor= document.getElementById("numeracion");
+    var letras=document.getElementsByClassName('letras-contenido');
+  
+    for (const letra of letras) {
+        letra.addEventListener('click',alerta);
+    }
+  
+    
+}
+function alerta(e){
+    console.log(e.target.nodeName)
+if(e.target.nodeName === 'DIV') {
+    var contenidoTextA=e.target.innerHTML.split(' ').map(e=>e.trim(' ')).filter(e=>e!="");
+    agregar(span(contenidoTextA), e.target);
 
-for(x=1;x<=20;x++){
-    var item=document.createElement('div');
-    item.classList.add("numero");
-    contenedor.appendChild(item);
-   
-    console.log("Item "+x);
 }
+
+ 
 }
+
+function span(contenidoTextA){
+    var cont = [];
+
+  
+    for(let e of contenidoTextA){
+        var item=document.createElement('span');
+        item.innerText=e;
+        cont.push(item);
+    } 
+
+    return(cont);
+
+}
+function agregar(arreglo,target){
+    var text=document.createElement('TEXTAREA');
+    text.addEventListener('keypress', sumar)
+    text.classList.add("textarea");
+    
+    for (const iterator of arreglo) {
+        text.innerText += iterator.innerText + ' ';
+        console.log(iterator);
+        
+    }
+    target.innerHTML="";
+    target.appendChild(text);
+    
+
+
+}
+
+function sumar(e) {
+    if(e.key === 'Enter') {
+        insertAfter(document.createElement('div'), e);
+    }
+}
+
+function insertAfter(e,i){ 
+    console.log(e, i)
+    if(e.nextSibling){ 
+        e.parentElement.insertBefore(i,e.nextSibling); 
+    } else { 
+        e.parentElement.appendChild(i); 
+    }
+}
+    
