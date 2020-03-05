3f5f3013485cf6e1a81fde1b55eb51616433b97e
Uriel Medina <urimeba511@gmail.com>
2020-03-05 09:37:15 -0600

    *feat* Models in Ejercicios
    Have been added the models for the 'Ejercicios' App. Also has changed the BD settings to work in MySQL

--- Apps/Ejercicios/models.py
+++ Apps/Ejercicios/models.py
@@ -1,14 +1,32 @@
 from django.db import models
+from Apps.Clases import models as models_clases
+from django.core.validators import MaxValueValidator, MinLengthValidator, MinValueValidator
 
 # # Create your models here.
-# class Ejercicio(models.Model):
 
-# class Intentos(models.Model):
+class Intentos(models.Model):
+    alumno = models.ForeignKey(models_clases.Alumno, on_delete=models.CASCADE)
+    ejercicio = models.ForeignKey('Ejercicio', on_delete=models.CASCADE)
 
-# class tipoEjercicio(models.Model):
+class Ejercicio(models.Model):
+    unidad = models.ForeignKey(models_clases.Unidad, on_delete=models.CASCADE)
+    tipo = models.ForeignKey('tipoEjercicio', on_delete=models.CASCADE)
+    is_activated = models.BooleanField(default=False, blank=False, null=False)
+    description = models.CharField(max_length=500, blank=True, null=True)
 
-# class Pregunta(models.Model):
+class tipoEjercicio(models.Model):
+    descripcion = models.CharField(max_length=200, blank=False, null=False)
+    intentos = models.IntegerField(default=3, validators=[MinValueValidator(1), MaxValueValidator(1)], blank=False, null=False)
 
-# class OpcionRespuesta(models.Model):
+class Pregunta(models.Model):
+    ejercicio = models.ForeignKey('Ejercicio', on_delete=models.CASCADE)
+    pregunta = models.CharField(max_length=300, blank=False, null=True)
 
-# class Respuesta(models.Model):
+class OpcionRespuesta(models.Model):
+    pregunta = models.ForeignKey('Pregunta', on_delete=models.CASCADE)
+    is_correct = models.BooleanField(default=False, blank=False, null=True)
+
+class Respuesta(models.Model):
+    pregunta = models.ForeignKey('Pregunta', on_delete=models.CASCADE)
+    alumno = models.ForeignKey(models_clases.Alumno, on_delete=models.CASCADE)
+    respuesta = models.CharField(max_length=200, blank=True, null=True)
--- database/settings.py
+++ database/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'database.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'database',
+        'HOST': '127.0.0.1',
+        'USER': 'root',
+        'PASSWORD': '',
+        'PORT': '3306'
     }
 }
 
