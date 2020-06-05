5929eb22aa14eac4b7d97d7d1681b14f7472def7
Uriel Medina <urimeba511@gmail.com>
2020-06-04 19:06:14 -0500

    *fix* Whitenoise

--- database/settings.py
+++ database/settings.py
@@ -123,7 +123,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR, 'Static'),]
 STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles/')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_URL = '/Templates/'
